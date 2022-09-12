d341b9cb29bba15c65a8e6f80c3bcc3a1d83b110
Pranitha-24 <109952550+Pranitha-24@users.noreply.github.com>
2022-09-12 16:03:39 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,10 +4,10 @@ def insertionSort(array) -> List[int]:
     for n in range(1,len(array)):
         key = array[n]
         num = n-1
-        while num >= 0 and key < array[num]
+        while num >= 0 and key < array[num]:
             array[num + 1] = array[num]
             num -= 1
-        array[num + 1] = key
+        array[num + 1] = 0
     return array
 # data = [9, 5, 1, 4, 3]
 input_data = input()
